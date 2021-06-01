123c9e042bfe773f7d2c92914eecf2634db1bbe1
swan4er <42721152+swan4er@users.noreply.github.com>
2021-06-01 23:04:33 +0300

    Update MyProject3-HTML5-Shipping.data.js

--- MyProject3-HTML5-Shipping.data.js
+++ MyProject3-HTML5-Shipping.data.js
@@ -18,8 +18,8 @@ Module.expectedDataFileDownloads++;
     } else {
       throw 'using preloaded data can only be done on a web page or in a web worker';
     }
-    var PACKAGE_NAME = 'D:/Epic Games/Projects/MyProject3/Binaries/HTML5/MyProject3-HTML5-Shipping.data';
-    var REMOTE_PACKAGE_BASE = 'MyProject3-HTML5-Shipping.data';
+    var PACKAGE_NAME = 'https://dl.dropboxusercontent.com/s/ifk9wpo7ia1yc48/MyProject3-HTML5-Shipping.data?dl=0';
+    var REMOTE_PACKAGE_BASE = 'https://dl.dropboxusercontent.com/s/ifk9wpo7ia1yc48/MyProject3-HTML5-Shipping.data?dl=0';
     if (typeof Module['locateFilePackage'] === 'function' && !Module['locateFile']) {
       Module['locateFile'] = Module['locateFilePackage'];
       err('warning: you defined Module.locateFilePackage, that has been renamed to Module.locateFile (using your locateFilePackage for now)');
